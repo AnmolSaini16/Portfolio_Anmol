049934cf5a1dd7b94d501ea6484bcc69cfc699d0
AnmolSaini16 <72123368+AnmolSaini16@users.noreply.github.com>
2022-10-02 20:54:38 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Title from "./components/Title";
+import { DarkthemeContext } from "./Contexts/ThemeContext";
 
 function App() {
+  const getThemeInStorage = () => {
+    const item = localStorage.getItem("dark");
+    return JSON.parse(item);
+  };
+  const [dark, setDark] = useState(getThemeInStorage || true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DarkthemeContext.Provider value={{ dark, setDark }}>
+      <div className={`${!dark ? "home__light" : "home__dark"}`}>
+        <Nav />
+        <Title />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+        <Footer />
+      </div>
+    </DarkthemeContext.Provider>
   );
 }
 
